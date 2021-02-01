935fb437b82cad47f73d002e21b47ff0583e0b01
TY LIU <ltyzzz@ruc.edu.cn>
2021-02-02 01:34:20 +0800

    Update __init__.py

--- Daisy/data/__init__.py
+++ Daisy/data/__init__.py
@@ -2,4 +2,4 @@ from .dataset import Dataset
 from .iterator import Iterator
 from .field import NumericalField, CategoricalField
 from .process import get_dataset
-from .simulator import create_gaussian_oracle, gaussian_simulator, TreeMaker, FCMaker, GeneralMaker, ChainMaker
+#from .simulator import create_gaussian_oracle, gaussian_simulator, TreeMaker, FCMaker, GeneralMaker, ChainMaker
